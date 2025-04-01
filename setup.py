e77e5999d24036bec339a98bdc25e95bde47b6ed
unknown <durgeshsingh12712@gmail.com>
2025-04-01 14:34:47 +0530

    Setup.py & data_ingestion Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "MLPipeline",
+    version= "0.0.1",
+    author= "Durgesh",
+    author_email= "durgeshsingh12712@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
